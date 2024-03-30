5c277b3a1a3e85c78f1b70afe98e8e5122c3ab3f
Anthony <zmyantro@gmail.com>
2024-03-30 16:20:48 -0500

    Created basic sign in screen

--- app/src/main/java/edu/myutsa/two_note/MainActivity.java
+++ app/src/main/java/edu/myutsa/two_note/MainActivity.java
@@ -7,11 +7,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.annotation.Nullable;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
