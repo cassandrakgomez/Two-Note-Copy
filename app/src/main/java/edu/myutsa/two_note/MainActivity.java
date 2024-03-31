40816bc72cd5f0b24671a15e20cd85fb9dc5da75
Anthony <zmyantro@gmail.com>
2024-03-30 21:12:48 -0500

    Created two activitys, with functionality to switch between them
    One is a login page (activity_main.xml) and the other is the landing page(landing_page.xml)

--- app/src/main/java/edu/myutsa/two_note/MainActivity.java
+++ app/src/main/java/edu/myutsa/two_note/MainActivity.java
@@ -1,8 +1,12 @@
 package edu.myutsa.two_note;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.ComponentActivity;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -10,7 +14,7 @@ import androidx.core.view.WindowInsetsCompat;
 import androidx.annotation.Nullable;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button button;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +24,17 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        setupButtons();
+    }
+    private void setupButtons() {
+        button = findViewById(R.id.signInButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, NoteSpaceActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
---
+++ app/src/main/java/edu/myutsa/two_note/NoteSpaceActivity.java
@@ -0,0 +1,20 @@
+package edu.myutsa.two_note;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.annotation.Nullable;
+
+public class NoteSpaceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.landing_page);
+
+    }
+}
